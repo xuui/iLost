c68735fff696423eadd4105e71fa588a3ba6faef
Xu.hel <xu.hel@outlook.com>
2015-08-12 23:19:10 +0800

    make ilost_comment_form

--- comments.php
+++ comments.php
@@ -12,6 +12,46 @@ if(!comments_open()){?>
   <div class="alert alert-warning" role="alert"><?php _e('Comments are closed.','iLost');?></div>
 <?php }}
 require_once(TEMPLATEPATH.'/include/smiley.php');
-comment_form('comment_notes_after=<p class="smiley">'.$smilies.'</p>');
+$ilost_comment_fields=array(
+  'author'=>'<div class="form-group form-group-sm comment-form-author"><label class="control-label" for="author">'.__('Name').($req ? '<span class="required">*</span>':'').'</label> '.'<input type="text" class="form-control" id="author" name="author" placeholder="your name" value="'.esc_attr($commenter['comment_author']).'" size="30"'.$aria_req.' /></div>',
+  'email'=>'<div class="form-group form-group-sm comment-form-email"><label class="control-label" for="email">'.__('Email').($req ? '<span class="required">*</span>':'').'</label>'.'<input type="text" class="form-control" id="email" name="email" placeholder="your@mail.com" value="'.esc_attr($commenter['comment_author_email']).'" '.$aria_req.' /></div>',
+  'url'=>'<div class="form-group form-group-sm comment-form-url"><label class="control-label" for="url">'.__('Website').'</label>'.'<input type="text" class="form-control" id="url" name="url" placeholder="http:///" value="'.esc_attr($commenter['comment_author_url']).'" size="30" /></div>',
+);
+$ilost_comment_form=array(
+  'class_submit'=>'btn btn-primary',
+  'comment_field'=>'<div class="form-group form-group-sm comment-form-comment"><label class="control-label" for="comment">'._x('Comment','noun').'</label><textarea class="form-control" id="comment" name="comment" placeholder="o ... .. ." rows="6" aria-required="true">'.'</textarea></div>',
+  //'comment_notes_after'=>'<p class="smiley">'.$smilies.'</p>'.'<p class="help-block form-allowed-tags">'.sprintf(__('You may use these <abbr title="HyperText Markup Language">HTML</abbr> tags and attributes: %s'),'<code>'.allowed_tags().'</code>').'</p>',
+  'comment_notes_after'=>'<p class="form-group smiley">'.$smilies.'</p>',
+  
+  'fields'=>apply_filters('comment_form_default_fields',$ilost_comment_fields),
+  /*
+  'id_form'           => 'commentform',
+  'title_reply'       => __( 'Leave a Reply' ),
+  'title_reply_to'    => __( 'Leave a Reply to %s' ),
+  'cancel_reply_link' => __( 'Cancel Reply' ),
+  'label_submit'      => __( 'Post Comment' ),
+  'format'            => 'xhtml',
+  'id_submit'         => 'submit',
+  'name_submit'       => 'submit',
+
+  'must_log_in' => '<p class="must-log-in">' .sprintf(
+      __( 'You must be <a href="%s">logged in</a> to post a comment.' ),
+      wp_login_url( apply_filters( 'the_permalink', get_permalink() ) )
+    ) . '</p>',
+
+  'logged_in_as' => '<p class="logged-in-as">' .sprintf(
+    __( 'Logged in as <a href="%1$s">%2$s</a>. <a href="%3$s" title="Log out of this account">Log out?</a>' ),
+      admin_url( 'profile.php' ),
+      $user_identity,
+      wp_logout_url( apply_filters( 'the_permalink', get_permalink( ) ) )
+    ) . '</p>',
+
+  'comment_notes_before' => '<p class="comment-notes">' .
+    __( 'Your email address will not be published.' ) . ( $req ? $required_text : '' ) .
+    '</p>',
+
+  */
+);
+comment_form($ilost_comment_form);
 ilost_ctrlentry();?>
 </div>
