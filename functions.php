547b52e40c6d5fcb6935b75cc05df700feb5b5f7
Xu.hel <xu.hel@outlook.com>
2015-12-28 15:00:44 +0800

    远程图片本地化
    Signed-off-by: Xu.hel <xu.hel@outlook.com>

--- functions.php
+++ functions.php
@@ -37,6 +37,7 @@ if(!function_exists('ilost_comments')){function ilost_comments($comment,$args,$d
 }}
 
 //login_Viewer.
+/*
 add_action('post_submitbox_misc_actions','login_Viewer');
 add_action('save_post','save_login_View_meta');
 function login_Viewer(){
@@ -66,4 +67,87 @@ function save_login_View_meta($post_id){
         delete_post_meta($post_id,'viewsLevel','');
     }
 }
+*/
+
+    // 远程图片本地化
+    add_filter('content_save_pre', 'auto_save_image');
+    function auto_save_image($content){
+
+    $text = stripslashes($content);
+    preg_match_all("/<img[^>]+src=(\"|\'){0,}(http:\/\/(.+?))(\"|\'|\s)/is", $text, $img);
+    $img = array_unique($img[2]);
+
+    if(empty($img)) {
+    return $content;
+    }
+
+    $upload_dir = wp_upload_dir(date('Y/m'));
+
+    require_once (ABSPATH . "wp-includes/class-snoopy.php");
+    $snoopy_Auto_Save_Image = new Snoopy;
+
+    // 过滤下载域名
+    $basehost = $_SERVER["HTTP_HOST"]."|.qq.com|img.baidu.com|.qiniudn.com|.taobaocdn.com|.alicdn.com|.gtimg.com";
+
+    // 以文章的标题作为图片的标题
+    if (!empty($_REQUEST['post_title'])) {
+    $post_title = wp_specialchars(stripslashes($_REQUEST['post_title']));
+    }
+
+    foreach ($img as $key => $imgurl){
+    set_time_limit(60); //每张图片允许下载最长时间,秒
+
+    if(preg_match("/$basehost/i", $imgurl)) {
+    continue;
+    }
+
+    // 是否本地图片
+    if(!preg_match("/^http:\/\//i", $imgurl)) {
+    continue;
+    }
+
+    // 判断后缀
+    $fileext = substr(strrchr($imgurl, '.'), 1);
+    $fileext = strtolower($fileext);
+    $savefiletype = array('jpg', 'gif', 'png', 'bmp');
+    if (!in_array($fileext, $savefiletype)){
+    $fileext = "jpg";
+    }
+
+    if($snoopy_Auto_Save_Image->fetch($imgurl)){
+    $get_file = $snoopy_Auto_Save_Image->results;
+    }else{
+    continue;
+    }
+
+    $filename = substr(md5($imgurl), 0, 8);
+    $filetarget = $upload_dir['path'] . "/" . $filename . "." . $fileext;
+
+    if(!@$fp = fopen($filetarget, 'wb')) {
+    continue;
+    } else {
+    flock($fp, 2);
+    fwrite($fp, $get_file);
+    fclose($fp);
+    }
+
+    $post_ID = (int)$_POST['temp_ID2'];
+    $wp_filetype = wp_check_filetype($filename . "." . $fileext, false);
+    $url = $upload_dir['url'] . "/" . $filename . "." . $fileext;
+
+    $attachment = array('post_type' => 'attachment',
+    'post_mime_type' => $wp_filetype['type'],
+    'guid' => $url,
+    'post_parent' => $post_ID,
+    'post_title' => $post_title,
+    'post_content' => "",
+    );
+    $id = wp_insert_attachment($attachment, $filetarget);
+    $text = str_replace($imgurl, $url, $text);
+
+    }
+    $content = addslashes($text);
+    remove_filter('content_save_pre', 'auto_save_image');
+    return $content;
+    }
 ?>
