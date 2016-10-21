cc993be54f0c3a7f258a0a692cdb297aebc1674f
Xu.hel <xu.hel@outlook.com>
2016-10-21 23:36:54 +0800

    front-page 完善
    Signed-off-by: Xu.hel <xu.hel@outlook.com>

--- footer.php
+++ footer.php
@@ -1,27 +1,42 @@
 <footer>
-<div class="container">
+  <div class="container">
+    <ul class="row">
+    <?php if(!dynamic_sidebar('footer-sidebar')){$ilost_widgerFooter=array('before_widget'=>'<li class="widget col-xl-3 col-xs-3 col-md-3 col-sm-3">','after_widget'=>'</li>');
+      the_widget('ilost_footlistsWidget','catid=1&number=5',$ilost_widgerFooter);
+      the_widget('ilost_footlistsWidget','catid=1&number=5',$ilost_widgerFooter);
+      the_widget('ilost_footlistsWidget','catid=3&number=5',$ilost_widgerFooter);
+      the_widget('ilost_footlistsWidget','catid=4&number=5',$ilost_widgerFooter);
+      the_widget('ilost_footlistsWidget','catid=5&number=5',$ilost_widgerFooter);
+      the_widget('ilost_footlistsWidget','catid=6&number=5',$ilost_widgerFooter);
+    }?>
+    </ul>
+    <div class="copyrt">
+      <p class="line"><span class="alignright"><?php printf('<a href="'.ilost_wp_homeurl.'/'.'">HOME</a>'.__(' &brvbar; '));?><a href="<?php echo ilost_wp_rss2_url;?>"><?php echo esc_attr('RSS Feed');?></a></span><?php printf('<a href="'.esc_attr('http://creativecommons.org/licenses/by-nc-sa/3.0').'">Creative Commons BY-NC-SA</a>'.__(' &brvbar; ').'This is a premium theme'.__(' &brvbar; ').'Designed by <a href="'.esc_attr('http://xuui.net/').'">Xu.hel</a> in ChengDu.');?></p>
+      <p><span class="alignright"><?php wp_register('','');?></span><?php printf('Copyright &copy; 2006-2016 <a href="%1$s/" title="%2$s" rel="home">%2$s</a> &brvbar; Powered by <a href="http://wordpress.org/">WordPress</a>.',ilost_wp_homeurl,ilost_wp_name);?></p>
+    </div>
+
+  </div>
+<?php /*
+  <div class="container">
+    
+    <div class="row">
+      <div class="col-xl-122">
+      </div>
+    </div>
+  </div>
   <div id="gotop"><a href="javascript:;" onClick="ilosts.goTop();return false;" title="<?php _e('Back Top','iLost');?>"><?php _e('Back Top','iLost');?></a></div>
   <?php if(!is_front_page()){?>
-  <aside class="row  hidden-xs hidden-sm">
+  <aside class="row hidden-xs hidden-sm">
     <h3><?php _e(ilost_wp_name.' Archives','iLost');?> &raquo;</h3>
     <ul>
       <?php if(!dynamic_sidebar('footer-sidebar')){$ilost_widgerFooter=array('before_widget'=>'<li>','after_widget'=>'</li>');
-        the_widget('ilost_catlistsWidget','catid=1&number=5',$ilost_widgerFooter);
-        the_widget('ilost_catlistsWidget','catid=3&number=5',$ilost_widgerFooter);
-        the_widget('ilost_catlistsWidget','catid=4&number=5',$ilost_widgerFooter);
-        the_widget('ilost_catlistsWidget','catid=5&number=5',$ilost_widgerFooter);
-        the_widget('ilost_catlistsWidget','catid=6&number=5',$ilost_widgerFooter);
       }?>
     </ul>
     <i class="clearfix"></i>
   </aside>
   <?php }?>
-  <div class="cprt">
-    <p class="line"><span class="alignright"><?php printf('<a href="'.ilost_wp_homeurl.'/'.'">HOME</a>'.__(' &brvbar; '));?><a href="<?php echo ilost_wp_rss2_url;?>"><?php echo esc_attr('RSS Feed');?></a></span><?php printf('<a href="'.esc_attr('http://creativecommons.org/licenses/by-nc-sa/3.0').'">Creative Commons BY-NC-SA</a>'.__(' &brvbar; ').'This is a premium theme'.__(' &brvbar; ').'Designed by <a href="'.esc_attr('http://xuui.net/').'">Xu.hel</a> in ChengDu.');?></p>
-	<p><span class="alignright"><?php wp_register('','');?></span><?php printf('Copyright &copy; 2006-2015 <a href="%1$s/" title="%2$s" rel="home">%2$s</a> &brvbar; Powered by <a href="http://wordpress.org/">WordPress</a>.',ilost_wp_homeurl,ilost_wp_name);?>
-    </p>
-  </div>
 </div>
+*/?>
 </footer>
 <?php wp_footer();?>
 <!--<?php echo get_num_queries();?> queries. <?php timer_stop(1);?> seconds. -->
--- front-page.php
+++ front-page.php
@@ -1,34 +1,36 @@
 <?php /* Template Name: Home */
 get_header();?>
 <?php ilost_getiloshow();?>
+<div id="proaside">
 <?php $homempid=ilost_getfront();
 $shopics=get_post_meta($homempid,"shopimages",$single=false);if($shopics){
-  echo '<div id="proaside"><div class="container"><div class="row clear">';
+  echo '<div class="container"><div class="row">';
+  $col_class="col-xl-4 col-xs-4 col-md-4 col-sm-4";
+  //$col_class="col-xl-3 col-xs-3 col-md-3 col-sm-3";
   foreach($shopics as $shopic){$shopiclib=explode("###",$shopic);if(@$shopiclib[1]){$shoplink=$shopiclib[1];}else{$shoplink='#';}if($shopiclib[0]){
-    echo '<div class="col-xl-4 col-xs-4 col-md-4 col-sm-4 text-center"><a href="'.$shoplink.'" style="background-image:url('.$shopiclib[0].')"><img src="" alt=""/></a></div>';
+    echo '<div class="'.$col_class.'"><a href="'.$shoplink.'" style="background-image:url('.$shopiclib[0].')"><span>span</span></a></div>';
   }
-}echo '</div></div></div>';}?>
-<div id="row" class="row clear">
-  <article class="col-xl-9 col-md-9 col-sm-8 col-xs-12 home<?php if(ilost_getsidefl()=='left')echo ' pull-right'?>">
-    <?php if(!dynamic_sidebar('home-listbar')){
-      //the_widget('ilost_querycatsWidget','catid=4&number=6');
-      //the_widget('ilost_querycatsWidget','catid=5&number=6');
-    }?>
-  </article>
-  
-  <aside class="col-xl-3 col-md-3 col-sm-4 col-xs-12 modhome">
-    <ul id="sidehome" class="clearfix">
-    <?php if(!dynamic_sidebar('home-sidebar')){
-      //the_widget('ilost_randompostWidget','number=6',$ilost_widgerSide);
-      //the_widget('WP_Widget_Recent_Posts','number=6',$ilost_widgerSide);
-      //if(function_exists('get_most_viewed')){the_widget('ilost_viewsWidget','number=6',$ilost_widgerSide);}
-      //the_widget('WP_Widget_Categories','',$ilost_widgerSide);
-      //the_widget('WP_Widget_Meta','',$ilost_widgerSide);
-      }?>
-    </ul>
-  </aside>
-  <i class="clearfix"></i>
+}echo '</div></div>';}?>
 </div>
+<div id="newpost">
+  <div class="container">
+    <div class="row">
+      <?php ilost_queryNewp($limit=4);?>
+    </div>
+  </div>
+</div>
+<?php /*
+<?php if(!dynamic_sidebar('home-listbar')){
+  //the_widget('ilost_querycatsWidget','catid=4&number=6');
+  //the_widget('ilost_querycatsWidget','catid=5&number=6');
+}?>
+<?php if(!dynamic_sidebar('home-sidebar')){
+  //the_widget('ilost_randompostWidget','number=6',$ilost_widgerSide);
+  //the_widget('WP_Widget_Recent_Posts','number=6',$ilost_widgerSide);
+  //if(function_exists('get_most_viewed')){the_widget('ilost_viewsWidget','number=6',$ilost_widgerSide);}
+  //the_widget('WP_Widget_Categories','',$ilost_widgerSide);
+  //the_widget('WP_Widget_Meta','',$ilost_widgerSide);
+}?>
 front-page
-
+*/?>
 <?php get_footer();?>
