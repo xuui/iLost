f10072175522c84aa8d54b9015752767dd09bacf
Xu.hel <xu.hel@outlook.com>
2017-06-09 23:08:40 +0800

    存档页更新
    Signed-off-by: Xu.hel <xu.hel@outlook.com>

--- archive.php
+++ archive.php
@@ -1,7 +1,7 @@
 <?php get_header();?>
 <div class="breadwarp">
   <div class="container">
-      <?php ilost_breadcrumb();?>
+      <?php breadcrumb_simple();//ilost_breadcrumb();?>
   </div>
 </div>
 <div class="contwarp contindex">
@@ -11,31 +11,28 @@
       <?php if(have_posts()){ilost_breadcrumb();?>
       <?php while(have_posts()){the_post();?>
         <section id="post-<?php the_ID();?>" <?php post_class();?>>
+        <?php if(has_post_thumbnail()){
+        $img_src=wp_get_attachment_image_src(get_post_thumbnail_id($post->ID),'full');
+        echo '<div class="thumg" style="background-image:url('.$img_src[0].')"></div>';
+        }?>
+        <div class="inarp">
           <div class="title">
             <h2><a href="<?php the_permalink();?>" title="<?php printf(esc_attr__('Permalink to %s','iLost'),the_title_attribute('echo=0'));?>" rel="bookmark"><?php the_title();?></a></h2>
-            <small><?php the_time('m.d.Y');?>, <?php comments_popup_link(__('No Comments','iLost'),__('1 Comment','iLost'),__('% Comments','iLost'));?>, <?php the_category(', ');?>, by <?php the_author_posts_link();?><?php if(function_exists('the_views')){?>, <?php the_views();}?>.<?php edit_post_link(__('Edit','iLost'),' [',']&#187;');?></small>
+            <small><?php the_time('m.d.Y');?>, <?php comments_popup_link(__('0','iLost'),__('1','iLost'),__('%','iLost'),'post-comments',__('-','iLost'));?>, <?php the_category(', ');?>, by <?php the_author_posts_link();?><?php if(function_exists('the_views')){?>, <?php the_views();}?>.<?php edit_post_link(__('Edit','iLost'),' [',']&#187;');?></small>
           </div>
           <div class="entry">
-            <?php the_post_thumbnail('thumbnail');
+            <?php //the_post_thumbnail('thumbnail');
             the_excerpt();?>
           </div>
           <div class="post-meta">
             <?php edit_post_link(__('Edit','iLost'),'<span class="alignright">[',']</span>');?><?php the_tags(__('Tags: ','iLost'),' | ','');?>&nbsp;<i class="clearfix"></i>
           </div>
+        </div>
         </section>
         <?php }?>
         <nav class="navigation">
           <?php ilost_pagenav();?>
         </nav>
-        <?php }else{?>
-        <section>
-          <div class="title"><h2><?php _e('Not Found','iLost');?></h2></div>
-          <div class="entry">
-            <p><?php _e('Sorry, but you are looking for something that isn\'t here.','iLost');?></p>
-            <p><?php _e('Apologies, but no results were found for the requested archive. Perhaps searching will help find a related post.','iLost');?></p>
-            <?php get_search_form();?>
-          </div>
-        </section>
         <?php }?>
     </article>
     <?php get_sidebar();?>
