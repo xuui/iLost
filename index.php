3ed95fe60ac9747d72f3321a675a6feb86d47ed3
Xu.hel <xu.hel@outlook.com>
2017-06-09 23:07:38 +0800

    博客列表更新
    Signed-off-by: Xu.hel <xu.hel@outlook.com>

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php get_header();?>
 <div class="breadwarp">
   <div class="container">
-      <?php ilost_breadcrumb();?>
+      <?php //ilost_breadcrumb();?>
   </div>
 </div>
 <?php /*$blogpageID=ilost_getposts();if($blogpageID){?>
@@ -22,17 +22,36 @@
         echo '<div class="thumg" style="background-image:url('.$img_src[0].')"></div>';
         }?>
         <div class="inarp">
-        <div class="title">
-          <h2><a href="<?php the_permalink();?>" title="<?php printf(esc_attr__('Permalink to %s','iLost'),the_title_attribute('echo=0'));?>" rel="bookmark"><?php the_title();?></a></h2>
-          <small><?php the_time('m.d.Y');?>, <?php comments_popup_link(__('No Comments','iLost'),__('1 Comment','iLost'),__('% Comments','iLost'));?>, <?php the_category(', ');?>, by <?php the_author_posts_link();?><?php if(function_exists('the_views')){?>, <?php the_views();}?>.<?php edit_post_link(__('Edit','iLost'),' [',']&#187;');?></small>
+          <div class="title">
+            <h2><a href="<?php the_permalink();?>" title="<?php printf(esc_attr__('Permalink to %s','iLost'),the_title_attribute('echo=0'));?>" rel="bookmark"><?php the_title();?></a></h2>
+            <small>
+              <span class="post-data pull-right">
+                <i class="fa fa-calendar-o"></i> <?php the_time('Y-m-d');?>
+              </span>
+              <span class="post-author">
+                <?php echo get_avatar(get_the_author_meta('user_email'),48);
+                the_author_posts_link();?>
+              </span>
+              <span class="post-data">
+                <i class="fa fa-comment-o"></i>
+                <?php comments_popup_link(__('0','iLost'),__('1','iLost'),__('%','iLost'),'post-comments',__('-','iLost'));?>
+              </span>
+              <?php /*
+              <i class="fa fa-eye-o"></i> <?php if(function_exists('the_views')){?><?php the_views();}?>
+              <i class="fa fa-folder-o"></i> <?php the_category(', ');?>, 
+              <i class="ion-person"></i>  <?php the_author_posts_link();?>
+               <?php edit_post_link(__('Edit','iLost'),'<i class="fa fa-pencil-square-o"></i> [',']&#187;');?>
+              */?>
+              
+            </small>
+          </div>
+          <div class="entry">
+            <?php the_excerpt();
+            //the_content(__('Learn more','iLost'));?>
+          </div>
         </div>
-        <div class="entry">
-          <?php the_content(__('Learn more','iLost'));?>
-        </div>
-        <div class="post-meta">
-          <?php edit_post_link(__('Edit','iLost'),'<span class="alignright">[',']</span>');?><?php the_tags(__('Tags: ','iLost'),' | ','');?>&nbsp;<i class="clearfix"></i>
-        </div>
-        </div>
+        <?php $posTag=get_the_tag_list('<span class="tag"><i class="ion-ios-pricetag-outline"></i>','</span> <span class="tag"><i class="ion-ios-pricetag-outline"></i>','</span>');
+        if($posTag){echo '<div class="post-meta">'.$posTag.'</div>';}?>
       </section>
       <?php }?>
       <nav>
