317739d810d304fbe2eff7c673ce8c75bf8272e9
Xu.hel <xu.hel@outlook.com>
2018-07-13 00:07:03 +0800

    fix comment

--- comments.php
+++ comments.php
@@ -1,9 +1,39 @@
+<?php
+if(post_password_required()){return;}
+?>
 
-<?php if(post_password_required()){?>
+<section id="comments" class="clearfix"><div class="inarp">
+	<?php if(have_comments()){?>
+		<h3 class="comments-title">
+			<?php $comments_number=get_comments_number();
+			if('1'===$comments_number){
+				/* translators: %s: post title */
+				printf(_x('One Reply to &ldquo;%s&rdquo;','comments title','iLost'),get_the_title());
+			}else{
+				printf(_nx('%1$s Reply to &ldquo;%2$s&rdquo;','%1$s Replies to &ldquo;%2$s&rdquo;',$comments_number,'comments title','iLost'),number_format_i18n($comments_number),get_the_title());
+			} ?>
+		</h2>
+
+		<ol class="comment-list">
+			<?php wp_list_comments(array('avatar_size'=>100,'style'=>'ol','short_ping'=>true,'reply_text'=>__('Reply','iLost')));?>
+		</ol>
+		<?php the_comments_pagination(array('prev_text'=>'<span class="screen-reader-text">'.__('Previous','iLost').'</span>','next_text'=>'<span class="screen-reader-text">'.__('Next','iLost').'</span>',));
+  } // Check for have_comments().
+
+	// If comments are closed and there are comments, let's leave a little note, shall we?
+	if (!comments_open() && get_comments_number() && post_type_supports( get_post_type(),'comments')){ ?>
+		<p class="no-comments"><?php _e( 'Comments are closed.', 'iLost' ); ?></p>
+	<?php }
+	comment_form();
+	?>
+</div></section>
+
+<?php /*
+if(post_password_required()){?>
 <div class="alert alert-warning" role="alert"><?php _e('This post is password protected. Enter the password to view any comments.','iLost');?></div>
 <?php return;}
 if(have_comments()){?>
-<section id="comments" class="clearfix"><div class="inarp">
+<section id="comments2" class="clearfix"><div class="inarp">
   <h3 id="comments-title"><?php printf(_n(__('One Response','iLost'),__('%1$s Responses','iLost'),get_comments_number()),number_format_i18n(get_comments_number()));?></h3>
   <ol class="comment-list media-list"><?php wp_list_comments(array('callback'=>'ilost_comments'));?></ol>
 <?php if(get_comment_pages_count()>1 && get_option('page_comments')){?>
@@ -29,3 +59,5 @@ if(!comments_open()){?>
 <div class="alert alert-warning" role="alert"><?php _e('Comments are closed.','iLost');?></div>
 <?php }
 }
+*/
+?>
