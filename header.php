0025c40004ae0059847838984a750a62534f030d
Xu.hel <xu.hel@outlook.com>
2018-08-03 20:02:31 +0800

    fix page seo

--- header.php
+++ header.php
@@ -10,41 +10,44 @@
 <meta name="renderer" content="webkit">
 <title><?php if(is_single()){single_post_title();ilost_page_number();}elseif(is_front_page()){echo ilost_wp_name;ilost_page_number();}elseif(is_page()||is_home()){wp_title('- '.ilost_wp_name,true,'right');ilost_page_number();}elseif(is_search()){printf(__('Search results for %s','iLost'),esc_html($s));ilost_page_number();}elseif(is_404()){echo __('Not Found','iLost');}else{echo wp_title('',false,'right').' - '.ilost_wp_name.ilost_page_number(false);}?></title>
 <?php if(is_single()){
-  $keywords=ilost_searchKey().', ';
+  $keywords=xuui_searchKey().', ';
   $tags=wp_get_post_tags($post->ID);
-  foreach($tags as $tag){$keywords=$keywords.$tag->name.', ';}
+  foreach($tags as $tag){$keywords==$post->post_title.', '.$keywords.$tag->name.', ';}
   if($post->post_excerpt){
-    $description=$post->post_excerpt;
+    $description=$post->post_title.' '.$post->post_excerpt;
   }else{
-    $description=ilost_substr(strip_tags($post->post_content),0,220);
+    $description=$post->post_title.' '.xuui_substr(strip_tags($post->post_content),0,120);
   }
   $description=str_replace("\n",' ',$description);
 }elseif(is_page()){
-  $keywords=ilost_wp_name.', '.ilost_searchKey().wp_title(',',false);
+  $keywords=xuui_wp_name.', '.xuui_searchKey().wp_title(',',false);
   $keywords=str_replace(' ,',',',$keywords);
-  if($post->post_excerpt){
-    $description=$post->post_excerpt;
+  if(!is_front_page()){
+    if($post->post_excerpt){
+      $description=$post->post_excerpt;
+    }else{
+      $description=xuui_substr(strip_tags($post->post_content),0,220);
+    }
+    $description=str_replace("\n",' ',$description);
   }else{
-    $description=ilost_substr(strip_tags($post->post_content),0,220);
+    if(xuui_seDescription()){
+      $description=xuui_seDescription();
+    }else{$description=xuui_wp_description;}
   }
-  $description=str_replace("\n",' ',$description);
 }elseif(is_category()||is_tag()){
-  $keywords=ilost_wp_name.', '.ilost_searchKey().wp_title(',',false);
+  $keywords=xuui_wp_name.', '.xuui_searchKey().wp_title(',',false);
   $keywords=str_replace(' ,',',',$keywords);
-  if(ilost_seDescription()){
-    $description=ilost_seDescription();
+  if(xuui_seDescription()){
+    $description=xuui_seDescription();
   }else{
-    $description=ilost_wp_description;
+    $description=xuui_wp_description;
   }
 }else{
-  $keywords=ilost_wp_name.', '.ilost_searchKey();
-  if(ilost_seDescription()){
-    $description=ilost_seDescription();
-  }else{
-    $description=ilost_wp_description;
-  }
-}
-?>
+  $keywords=xuui_wp_name.', '.xuui_searchKey();
+  if(xuui_seDescription()){
+    $description=xuui_seDescription();
+  }else{$description=xuui_wp_description;}
+}?>
 <meta name="keywords" content="<?php echo $keywords;?>">
 <meta name="description" content="<?php echo $description?>">
 <link rel="profile" href="http://gmpg.org/xfn/11">
